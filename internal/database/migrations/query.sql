c8a6f02e28dc631f96ad15de2cee59b32b9fcfa2
Vlad <zhevnyakvlig.1.23@gmail.com>
2024-07-17 21:35:42 +0300

    Add migrations

---
+++ internal/database/migrations/query.sql
@@ -0,0 +1,19 @@
+/*
+CREATE TABLE Orders (
+order_id INT NOT NULL PRIMARY KEY,
+order_date VARCHAR ( 30 ) NOT NULL DEFAULT '0000-00-00',
+customers_name VARCHAR ( 30 ) NOT NULL DEFAULT '-',
+product VARCHAR ( 255 ) NOT NULL,
+adress VARCHAR ( 255 ) NOT NULL,
+order_status VARCHAR ( 30 ) DEFAULT 'not delivered' 
+)
+*/
+
+
+/*
+INSERT INTO orders
+VALUES
+	( 1, '2024-07-18', 'Vlad', 'TV', 'Minsk', 'not delivered' ),
+	( 2, '2024-07-20', 'Stas', 'fridge', 'Minsk', 'delivered' ),
+	( 3, '2024-07-25', 'Kirill', 'washing machine', 'Minsk', 'failure' )
+    */
